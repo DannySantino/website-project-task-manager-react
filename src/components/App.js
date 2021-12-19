5e83a8bfc10d26c120ee3cef2cbee19177be5aad
Danny Santino <dannysantisimo@gmail.com>
2021-12-19 01:46:58 +0100

    add about page with router functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import TaskList from './TaskList';
 import TaskForm from './TaskForm';
+import Footer from './Footer';
+import About from './About';
 import TaskListContextProvider from '../context/TaskListContext';
 import '../App.css';
 
 const App = () => {
     return (
         <TaskListContextProvider>
-            <div className='container'>
-                <div className="app-wrapper">
-                    <div className="main">
-                        <Header />
-                        <TaskForm />
-                        <TaskList />
+            <Router>
+                <div className='container'>
+                    <div className="app-wrapper">
+                        <div className="main">
+                            <Header />
+                            <Routes>
+                                <Route path='/' exact element={
+                                    <>
+                                        <TaskForm />
+                                        <TaskList />
+                                    </>
+                                } />
+                                <Route path='/about' element={<About />} />
+                            </Routes>
+                            <Footer />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Router>
         </TaskListContextProvider>
     )
 }
